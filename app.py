5e877a0db079f8c1a1662ebd1631fedd93e650db
Keshav <Keshav1234567890qwerty@gmail.com>
2024-12-10 14:16:37 +1100

    home page path problem

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
-from views import views
+from flask import Flask,Blueprint, render_template, request, jsonify,redirect, url_for
 
 app = Flask(__name__)
-app.register_blueprint (views, url_prefix='/')
+@app.route ('/')
+def home():
+    return render_template("index.html")
+
+@app.route ('/game')
+def games():
+    return render_template("games.html")
+
+@app.route ('/movie')
+def moive():
+    return render_template("moives.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request, jsonify,redirect, url_for
-
-views = Blueprint(__name__, "views")
-
-@views.route ('/')
-def home():
-    return render_template("index1.html")
