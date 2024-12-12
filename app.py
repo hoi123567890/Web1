95f41f456f0348e2ede2832dfc27c4495632309c
Keshav <Keshav1234567890qwerty@gmail.com>
2024-12-13 09:47:13 +1100

    worked on login page and fixed games page and fixed home page

--- app.py
+++ app.py
@@ -6,12 +6,33 @@ def home():
     return render_template("index.html")
 
 @app.route ('/game')
-def games():
-    return render_template("games.html")
+def game():
+    return render_template("game.html")
+
+@app.route ('/game/<id>')
+def show_game():
+    game = load_games_from_db()
+    if not game:
+        return "Not found", 404
+    else:
+        return render_template("item.html", game=game)
 
 @app.route ('/movie')
-def moive():
-    return render_template("moives.html")
+def movie():
+    return render_template("movie.html")
+
+@app.route ('/movie/<id>')
+def show_moive():
+    moive = load_movie_from_db()
+    if not movie:
+        return "Not found", 404
+    else:
+        return render_template("item.html", game=movie)
+
+@app.route("/email/<id>" meathod=["post"])
+def email(id):
+    email=request.form
+    return jsonify.data
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
