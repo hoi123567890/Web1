6c9f08290548930894ef9b5725041a08fe2a8392
Keshav <Keshav1234567890qwerty@gmail.com>
2024-12-15 20:31:29 +1100

    added db.py

---
+++ .database/quries.sql
@@ -0,0 +1,21 @@
+--CREATE TABLE User(fristname TEXT NOT NULL,
+                  username TEXT NOT NULL,
+                  lastname TEXT NOT NULL,
+                  email TEXT NOT NULL,
+                  password TEXT NOT NULL);
+
+CREATE TABLE Media (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    releasedate TEXT,
+                    summary BLOB,
+                    genre TEXT,
+                    game INTEGER NOT NULL CHECK (game >= 0 AND game <= 1));
+
+CREATE TABLE Review (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    rating INTEGER NOT NULL CHECK (rating >= 0 AND rating <= 10),
+                    username TEXT NOT NULL,
+                    media_id INTEGER NOT NULL,
+                    comment BLOB,
+                    FOREIGN KEY (username) REFERENCES User(username),
+                    FOREIGN KEY (media_id) REFERENCES Media(id));
+
---
+++ db.py
@@ -0,0 +1,53 @@
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def GetDB():
+    db = sqlite3.connect(".database/one.db")
+    db.row_factory = sqlite3.Row
+    return db
+
+def load_games_from_db():
+    db = GetDB()
+    indi_game = db.execute("SELECT * FROM media WHERE game=1").fetchall()
+    db.close()
+    return indi_game
+
+def load_movie_from_db():
+    db = GetDB()
+    indi_movie = db.execute("SELECT * FROM media WHERE game=0").fetchall()
+    db.close()
+    return indi_movie
+
+def load_media_from_db(id):
+    db = GetDB()
+    media = db.execute("SELECT * FROM media WHERE id=?",(id,)).fetchone()
+    db.close()
+    return media
+
+def email_veri(email):
+    db = GetDB()
+    media = db.execute("SELECT * FROM user WHERE email=?",(email,)).fetchone()
+    db.close()
+    return media
+
+def pass_veri(email,password):
+    db = GetDB()
+    media = db.execute("SELECT * FROM user WHERE email=?",(email,)).fetchone()
+    if check_password_hash(media['password'], password):
+        x= db.execute("SELECT * FROM user WHERE email=?",(email,)).fetchone()
+        db.close()
+        return x['username']
+    else:
+        return False
+
+def register(email,pa,last, frist,name):
+    db = GetDB()
+    hash = generate_password_hash(pa)
+    try:
+        db.execute("INSERT INTO User(username, password, fristname, lastname,email) VALUES(?,?,?,?,?)", (name,hash,frist,last,email,))
+        db.commit()
+        return True   
+    except sqlite3.IntegrityError:
+        return "error"
+    finally:
+        db.close()
